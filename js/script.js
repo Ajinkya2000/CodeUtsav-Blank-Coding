8acb47b9e3f137e8ac79afe5dfb86b29a3426147
Ajinkya Deshpande <55014143+Ajinkya2000@users.noreply.github.com>
2020-01-16 10:29:51 +0530

    Initial files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//* CART ANIMATION
+const cart = $(".cart");
+cart.hover(
+  function() {
+    $(".fa-shopping-cart").addClass("cart-animation");
+  },
+  function() {
+    $(".fa-shopping-cart").removeClass("cart-animation");
+  }
+);
+
+//* NAV SLIDE
+const navSlide = () => {
+  const burger = $(".burger");
+  const navul = $(".nav ul");
+
+  burger.click(function() {
+    navul.toggleClass("nav-active");
+
+    burger.toggleClass("toggle");
+  });
+};
+navSlide();
